89f360d56fa3bb97ce3b793004186f9404ae4bbc
elyabe306 <elyabe.m@escolar.ifrn.edu.br>
2025-01-17 20:35:42 -0300

    views adicionada

--- api/views.py
+++ api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Users, Posts, Comments, Albums, Photos, Todos
+from .serializers import UsersSerializer, PostsSerializer, CommentsSerializer, AlbumsSerializer, PhotosSerializer, TodosSerializer
 
 # Create your views here.
+
+class UsersViewSet(viewsets.ModelViewSet):
+    
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+class PostsViewSet(viewsets.ModelViewSet):
+   
+    queryset = Posts.objects.all()
+    serializer_class = PostsSerializer
+
+class CommentsViewSet(viewsets.ModelViewSet):
+
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+class AlbumsViewSet(viewsets.ModelViewSet):
+
+    queryset = Albums.objects.all()
+    serializer_class = AlbumsSerializer
+
+class PhotosViewSet(viewsets.ModelViewSet):
+
+    queryset = Photos.objects.all()
+    serializer_class = PhotosSerializer
+
+class TodosViewSet(viewsets.ModelViewSet):
+
+    queryset = Todos.objects.all()
+    serializer_class = TodosSerializer
