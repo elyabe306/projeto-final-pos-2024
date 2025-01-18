d039cffd2facdf99bb85e07e81ebb7ae7fae3215
elyabe306 <elyabe.m@escolar.ifrn.edu.br>
2025-01-17 21:58:07 -0300

    rota aninhada post comment

--- config/urls.py
+++ config/urls.py
@@ -1,38 +1,28 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from api import views
 
 router = routers.DefaultRouter()
+
 router.register(r'users', views.UsersViewSet)
+
 router.register(r'posts', views.PostsViewSet)
+posts_router = routers.NestedDefaultRouter(router, r"posts", lookup="posts")
+posts_router.register(r"comments", views.CommentsViewSet)
+
 router.register(r'comments', views.CommentsViewSet)
 router.register(r'albums', views.AlbumsViewSet)
 router.register(r'photos', views.PhotosViewSet)
 router.register(r'todos', views.TodosViewSet)
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    
 ]
 
 urlpatterns += router.urls
+
+urlpatterns = router.urls + posts_router.urls
